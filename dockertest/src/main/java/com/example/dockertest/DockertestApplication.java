7bcfbf21d02f0db14e7726d1c6633f2b6430789c
sanjiv <sanjiv.javarnd@gmail.com>
2018-04-04 13:05:14 -0700

    initial commit

---
+++ dockertest/src/main/java/com/example/dockertest/DockertestApplication.java
@@ -0,0 +1,12 @@
+package com.example.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockertestApplication.class, args);
+	}
+}
